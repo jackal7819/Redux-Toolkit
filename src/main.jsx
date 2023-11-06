b04a07d9eed385a81f1ebadd42f5f5d3c4d1fb7f
Viktor Filippov <jackal3166@gmail.com>
2023-11-07 00:12:07 +0200

    Add store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import './index.css';
 
-import App from './App';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { store } from './store/store.js';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );

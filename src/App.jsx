e6fe5bf1fc10d87dd3e33cb4e0408e9b570ffecf
Viktor Filippov <jackal3166@gmail.com>
2023-11-08 21:19:18 +0200

    Create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Navbar from './components/Navbar';
+
 function App() {
-	return <h2>Redux Toolkit</h2>;
+	return (
+		<main>
+			<Navbar />
+			<h2>Redux Toolkit</h2>
+		</main>
+	);
 }
 
 export default App;
